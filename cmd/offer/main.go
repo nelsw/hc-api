c3142d0f6d4fb996b3b5c6bb2fd6c0839aa5c1bc
nelsw <connorvanelswyk@gmail.com>
2019-12-30 11:49:46 -0500

    improved readability and completeness

--- cmd/offer/main.go
+++ cmd/offer/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	. "hc-api/service"
 	"os"
+	"time"
 )
 
 type Offer struct {
@@ -21,42 +22,58 @@ type Offer struct {
 	ProductPrice int64  `json:"product_price"`
 	ProductQty   int    `json:"product_qty"`
 	ProductImg   string `json:"product_img,omitempty"`
-	OfferTotal   int64  `json:"offer_total"`
+	Total        int64  `json:"total"`
 	Created      string `json:"created"`
 }
 
 var tableName = os.Getenv("OFFER_TABLE")
 
 func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("REQUEST [%v]", request)
+	cmd := request.QueryStringParameters["cmd"]
+	body := request.Body
+	ip := request.RequestContext.Identity.SourceIP
+	session := request.QueryStringParameters["session"]
+	fmt.Printf("REQUEST cmd=[%s], ip=[%s], session=[%s], body=[%s]\n", cmd, ip, session, body)
 
-	var statusCode = 400
-	var body = "bad request"
+	switch request.QueryStringParameters["cmd"] {
 
-	if request.QueryStringParameters["cmd"] == "save" {
+	case "save":
+
 		var offer Offer
 		if err := json.Unmarshal([]byte(request.Body), &offer); err != nil {
-			statusCode = 502
-			body = err.Error()
-		} else if err := Put(offer, &tableName); err != nil {
-			statusCode = 500
-			body = err.Error()
-		} else {
-			statusCode = 200
-			b, _ := json.Marshal([]byte(`{"message":"success"}`))
-			body = string(b)
+			return BadGateway().Error(err).Build()
 		}
+
+		ip := request.RequestContext.Identity.SourceIP
+		session := request.QueryStringParameters["session"]
+
+		um, err := Invoke().Handler("User").IP(ip).CMD("find").Session(session).Build()
+		if err != nil {
+			return BadRequest().Error(err).Build()
+		}
+
+		id := fmt.Sprintf("%v", um["profile_id"])
+		pm, err := Invoke().Handler("UserProfile").IP(ip).Session(session).CMD("find").QSP("id", id).Build()
+		if err != nil {
+			return BadRequest().Error(err).Build()
+		}
+
+		offer.Email = fmt.Sprintf("%v", pm["email"])
+		offer.Phone = fmt.Sprintf("%v", pm["phone"])
+		offer.FirstName = fmt.Sprintf("%v", pm["first_name"])
+		offer.LastName = fmt.Sprintf("%v", pm["last_name"])
+		offer.Created = time.Now().UTC().Format(time.RFC3339)
+		offer.Total = int64(offer.ProductQty) * offer.ProductPrice
+
+		if err := Put(offer, &tableName); err != nil {
+			return InternalServerError().Error(err).Build()
+		} else {
+			return Ok().Build()
+		}
+
+	default:
+		return BadRequest().Build()
 	}
-
-	response := events.APIGatewayProxyResponse{
-		StatusCode: statusCode,
-		Headers:    map[string]string{"Access-Control-Allow-Origin": "*"},
-		Body:       body,
-	}
-
-	fmt.Printf("RESPONSE [%v]", response)
-
-	return response, nil
 }
 
 func main() {
