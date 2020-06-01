82bfeb7c0fc0719ddb18e84b50fc1617dd0b4628
nelsw <connorvanelswyk@gmail.com>
2020-06-01 17:41:56 -0400

    test data update

--- test/data.go
+++ test/data.go
@@ -2,12 +2,13 @@ package test
 
 const (
 	AddressId     = "NTkxIEVWRVJOSUEgU1QsIEFQVCAxNzA1LCBXRVNUIFBBTE0gQkVBQ0gsIEZMLCAzMzQwMS01Nzg0LCBVbml0ZWQgU3RhdGVz"
-	CookieValid   = "acc-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMjcuMC4wLjEiLCJleHAiOjE1ODA4NDc2NDQsImp0aSI6IjYzOGIxM2VmLWFiODQtNDEwYS1hYmIwLWM5ZmQ1ZGE0NWM2MiIsImlhdCI6MTU4MDc2MTI0NCwic3ViIjoiYXV0aG9yaXplIn0.A9V3uOEhFWjBa3V2iG-58faROPzZNd57ei4ZzkfcHBI; Expires=Tue, 04 Feb 2020 20:20:44 GMT"
+	CookieValid   = "acc-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMjcuMC4wLjEiLCJleHAiOjE1ODEwNDk4NDEsImp0aSI6IjYzOGIxM2VmLWFiODQtNDEwYS1hYmIwLWM5ZmQ1ZGE0NWM2MiIsImlhdCI6MTU4MDk2MzQ0MSwic3ViIjoiYXV0aG9yaXplIn0.FSU0W46_F3OWjkmaU1JqXmn6Oc17t6YqLvqrJVOspTU; Expires=Fri, 11 May 2020 07:20:41 GMT"
 	CookieExpired = "acc-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMjcuMC4wLjEiLCJleHAiOjE1ODAzMjA5OTUsImp0aSI6IjYzOGIxM2VmLWFiODQtNDEwYS1hYmIwLWM5ZmQ1ZGE0NWM2MiIsImlhdCI6MTU4MDIzNDU5NSwic3ViIjoiYXV0aG9yaXplIn0._1UKrWVQ-Em4orBatu5GjP6VUSoyeSLLOTtm6lG0Cfo; Expires=Wed, 29 Jan 2020 18:03:15 GMT"
 	CredId        = "connor@wiesow.com"
 	UserId        = "638b13ef-ab84-410a-abb0-c9fd5da45c62"
 	ProfileId     = "df3d6542-c828-49e9-a62c-cb6e67d5d730"
 	ProductId     = "53f2ebcb-1689-11ea-9a91-6a36cd23892f"
+	ProductId1    = ProductId
 	ProductId2    = "0c15864b-1d41-11ea-91cb-164c58ee6eaf"
 	PasswordId    = "84e5c552-3a40-432a-b5ca-d3ef7a78dee7"
 	PasswordText  = "Pass123!"
