84aa958eba8c0154158afffc7deb8ad560fd9bbc
nelsw <connorvanelswyk@gmail.com>
2020-06-02 06:53:20 -0400

    user implemented

--- handler/user/main.go
+++ handler/user/main.go
@@ -1,28 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/dgrijalva/jwt-go"
 	"os"
 	"sam-app/pkg/client/faas/client"
 	"sam-app/pkg/factory/apigwp"
+	"strings"
 )
 
 var table = os.Getenv("TABLE")
 
 func Handle(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	apigwp.LogRequest(r)
+	if r.Path != "add" && r.Path != "delete" {
+		return apigwp.Response(400, fmt.Errorf("bad path [%s]", r.Path))
+	} else if token, ok := r.QueryStringParameters["token"]; !ok {
+		return apigwp.Response(400, fmt.Errorf("no token provided"))
+	} else if csv, ok := r.QueryStringParameters["ids"]; !ok {
+		return apigwp.Response(400, fmt.Errorf("no ids provided"))
+	} else if col, ok := r.QueryStringParameters["col"]; !ok {
+		return apigwp.Response(400, fmt.Errorf("no col provided"))
+	} else {
 
-	if r.Path == "add" || r.Path == "delete" {
+		keyword := r.Path + " " + col
+		inspection := events.APIGatewayProxyRequest{
+			Path:                  "inspect",
+			QueryStringParameters: map[string]string{"token": token},
+		}
+		code, body := client.CallIt(inspection, "tokenHandler")
+		if code != 200 {
+			return apigwp.Response(code, body)
+		}
+
+		response := events.APIGatewayProxyResponse{}
+		_ = json.Unmarshal([]byte(body), &response)
+
+		if response.StatusCode != 200 {
+			return apigwp.Response(response.StatusCode, response.Body)
+		}
+
+		claims := jwt.StandardClaims{}
+		if err := json.Unmarshal([]byte(response.Body), &claims); err != nil {
+			return apigwp.Response(400, err)
+		}
+
+		ids := strings.Split(csv, ",")
 		m := map[string]interface{}{
 			"table":   table,
-			"ids":     r.QueryStringParameters["ids"],
-			"keyword": r.Path + " " + r.QueryStringParameters["col"],
+			"id":      claims.Id,
+			"ids":     ids,
+			"keyword": keyword,
 		}
 		return apigwp.Response(client.CallIt(&m, "repoHandler"))
 	}
-
-	return apigwp.Response(400, fmt.Errorf("nothing returned for [%v].\n", r))
 }
 
 func main() {
--- handler/user/main_test.go
+++ handler/user/main_test.go
@@ -5,7 +5,73 @@ import (
 	"testing"
 )
 
-func TestHandleBadRequest(t *testing.T) {
+func TestHandleNoIds400(t *testing.T) {
+	r := events.APIGatewayProxyRequest{
+		Path: "delete",
+		QueryStringParameters: map[string]string{
+			"col":   "orders",
+			"token": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJBdWRpZW5jZSBWYWx1ZSIsImV4cCI6MTU5MTE3OTA1NywianRpIjoiSWQgVmFsdWUiLCJpYXQiOjE1OTEwOTI2NTcsImlzcyI6Iklzc3VlciBWYWx1ZSIsInN1YiI6IlN1YmplY3QgVmFsdWUifQ.SeLP6owuc0WPJqRMXZAUgorwsm2MhqC7_0C_-CPcMXU; Expires=Wed, 03 Jun 2020 10:10:57 GMT",
+		},
+	}
+	if out, _ := Handle(r); out.StatusCode != 400 {
+		t.Fail()
+	}
+}
+
+func TestHandleNoCol400(t *testing.T) {
+	r := events.APIGatewayProxyRequest{
+		Path: "delete",
+		QueryStringParameters: map[string]string{
+			"ids":   "foo,bar",
+			"token": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJBdWRpZW5jZSBWYWx1ZSIsImV4cCI6MTU5MTE3OTA1NywianRpIjoiSWQgVmFsdWUiLCJpYXQiOjE1OTEwOTI2NTcsImlzcyI6Iklzc3VlciBWYWx1ZSIsInN1YiI6IlN1YmplY3QgVmFsdWUifQ.SeLP6owuc0WPJqRMXZAUgorwsm2MhqC7_0C_-CPcMXU; Expires=Wed, 03 Jun 2020 10:10:57 GMT",
+		},
+	}
+	if out, _ := Handle(r); out.StatusCode != 400 {
+		t.Fail()
+	}
+}
+
+func TestHandleNoToken400(t *testing.T) {
+	r := events.APIGatewayProxyRequest{
+		Path: "delete",
+		QueryStringParameters: map[string]string{
+			"ids": "foo,bar",
+		},
+	}
+	if out, _ := Handle(r); out.StatusCode != 400 {
+		t.Fail()
+	}
+}
+
+func TestHandleAdd200(t *testing.T) {
+	r := events.APIGatewayProxyRequest{
+		Path: "add",
+		QueryStringParameters: map[string]string{
+			"ids":   "foo,bar",
+			"col":   "orders",
+			"token": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJBdWRpZW5jZSBWYWx1ZSIsImV4cCI6MTU5MTE4MTI3MiwianRpIjoiMmY2YTdmM2YtYTQ2OS0xMWVhLWE2MWItNzY3ZTBlODViOTk1IiwiaWF0IjoxNTkxMDk0ODcyLCJpc3MiOiJJc3N1ZXIgVmFsdWUiLCJzdWIiOiJTdWJqZWN0IFZhbHVlIn0.wuFOf1mbNkgnPmz3_iIl-6UFlKw9AkO4IKkvDqFT4Tg; Expires=Wed, 03 Jun 2020 10:47:52 GMT",
+		},
+	}
+	if out, _ := Handle(r); out.StatusCode != 200 {
+		t.Fail()
+	}
+}
+
+func TestHandleDelete200(t *testing.T) {
+	r := events.APIGatewayProxyRequest{
+		Path: "delete",
+		QueryStringParameters: map[string]string{
+			"ids":   "foo,bar",
+			"col":   "orders",
+			"token": "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJBdWRpZW5jZSBWYWx1ZSIsImV4cCI6MTU5MTE4MTI3MiwianRpIjoiMmY2YTdmM2YtYTQ2OS0xMWVhLWE2MWItNzY3ZTBlODViOTk1IiwiaWF0IjoxNTkxMDk0ODcyLCJpc3MiOiJJc3N1ZXIgVmFsdWUiLCJzdWIiOiJTdWJqZWN0IFZhbHVlIn0.wuFOf1mbNkgnPmz3_iIl-6UFlKw9AkO4IKkvDqFT4Tg; Expires=Wed, 03 Jun 2020 10:47:52 GMT",
+		},
+	}
+	if out, _ := Handle(r); out.StatusCode != 200 {
+		t.Fail()
+	}
+}
+
+func TestHandleNoPath400(t *testing.T) {
 	r := events.APIGatewayProxyRequest{}
 	if out, _ := Handle(r); out.StatusCode != 400 {
 		t.Fail()
