32bd778885fba8dffdaa4088bb7b3631a649fd88
nelsw <connorvanelswyk@gmail.com>
2020-06-02 00:47:57 -0400

    product handler updates

--- handler/product/main.go
+++ handler/product/main.go
@@ -1,64 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"os"
 	"sam-app/pkg/client/repo"
 	"sam-app/pkg/factory/apigwp"
+	"sam-app/pkg/model/product"
 	"strings"
-)
-
-type Product struct {
-	Id          string   `json:"id"`
-	Category    string   `json:"category"`
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Price       int64    `json:"price"`
-	ImageUrls   []string `json:"image_urls"`
-	OwnerId     string   `json:"owner_id"`
-	AddressId   string   `json:"address_id"` // shipping departure location
-	Unit        string   `json:"unit"`       // LB, OZ, etc.
-	Weight      int64    `json:"weight"`
-	Stock       int8     `json:"stock"`
-}
-
-func (e *Product) ID() string {
-	return e.Id
-}
-
-func (*Product) TableName() string {
-	return table
-}
-
-func (e *Product) Validate() error {
-	if len(e.Name) < 2 {
-		return errName
-	}
-	return nil
-}
-
-var (
-	table   = os.Getenv("PRODUCT_TABLE")
-	errName = fmt.Errorf("product name must be at least 2 characters in length")
 )
 
 func Handle(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	apigwp.LogRequest(r)
 
-	csv, ok := r.QueryStringParameters["ids"]
-	if ok {
-		ids := strings.Split(csv, ",")
-		if out, err := repo.FindMany(&Product{}, ids); err != nil {
-			return apigwp.Response(404, err)
+	switch r.Path {
+
+	case "save":
+		p := product.Entity{}
+		if err := json.Unmarshal([]byte(r.Body), &p); err != nil {
+			return apigwp.Response(400, err)
+		} else if err := repo.SaveOne(&p); err != nil {
+			return apigwp.Response(500, err)
 		} else {
-			return apigwp.Response(200, &out)
+			return apigwp.Response(200, &p)
 		}
+
+	case "remove":
+		if id, ok := r.QueryStringParameters["id"]; ok {
+			p := product.Entity{}
+			if err := repo.Remove(&p, id); err != nil {
+				return apigwp.Response(500, err)
+			} else {
+				return apigwp.Response(200, &p)
+			}
+		}
+
+	case "find":
+		if csv, ok := r.QueryStringParameters["ids"]; ok {
+			ids := strings.Split(csv, ",")
+			if out, err := repo.FindMany(&product.Entity{}, ids); err != nil {
+				return apigwp.Response(500, err)
+			} else {
+				return apigwp.Response(200, &out)
+			}
+		} else if id, ok := r.QueryStringParameters["id"]; ok {
+			p := product.Entity{Id: id}
+			if err := repo.FindOne(&p); err != nil {
+				return apigwp.Response(500, err)
+			} else {
+				return apigwp.Response(200, &p)
+			}
+		}
+
 	}
 
-	return apigwp.Response(400, fmt.Errorf("...bad request, reached EOF.\n"))
+	return apigwp.Response(400, fmt.Errorf("nothing returned for [%v].\n", r))
 }
 
 func main() {
--- handler/product/main_test.go
+++ handler/product/main_test.go
@@ -1,33 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
+	"sam-app/pkg/model/product"
 	"sam-app/test"
 	"testing"
 )
 
-var (
-	requestCtx = events.APIGatewayProxyRequestContext{
-		Identity: events.APIGatewayRequestIdentity{
-			SourceIP: test.Ip,
-		},
-	}
-)
-
 func TestHandleFindMany(t *testing.T) {
-	ids := test.ProductId1 + "," + test.ProductId2
-	r := events.APIGatewayProxyRequest{
-		QueryStringParameters: map[string]string{
-			"ids": ids,
-		},
-		RequestContext: requestCtx,
-	}
-
-	out, _ := Handle(r)
-	if out.StatusCode != 200 {
+	r := events.APIGatewayProxyRequest{Path: "find", QueryStringParameters: map[string]string{"ids": test.ProductIds}}
+	if out, _ := Handle(r); out.StatusCode != 200 {
 		t.Fail()
 	}
+}
 
+func TestHandleFindOne(t *testing.T) {
+	r := events.APIGatewayProxyRequest{Path: "find", QueryStringParameters: map[string]string{"id": test.ProductId}}
+	if out, _ := Handle(r); out.StatusCode != 200 {
+		t.Fail()
+	}
+}
+
+func TestHandleSaveOne(t *testing.T) {
+	p := product.Entity{
+		Category:    "Category Value",
+		Name:        "Name Value",
+		Description: "Description Value",
+		Price:       99999999999,
+		ImageUrls:   nil,
+		OwnerId:     "OwnerId Value",
+		AddressId:   "AddressId Value",
+		Unit:        "Unit Value",
+		Weight:      9999,
+		Stock:       999999,
+	}
+	b, _ := json.Marshal(&p)
+	r := events.APIGatewayProxyRequest{Path: "save", Body: string(b)}
+	if out, _ := Handle(r); out.StatusCode != 200 {
+		t.Fail()
+	}
+}
+
+func TestHandleDeleteOne(t *testing.T) {
+	r := events.APIGatewayProxyRequest{Path: "remove", QueryStringParameters: map[string]string{"id": test.ProductId}}
+	if out, _ := Handle(r); out.StatusCode != 200 {
+		t.Fail()
+	}
+}
+
+func TestHandleBadRequest(t *testing.T) {
+	r := events.APIGatewayProxyRequest{}
+	if out, _ := Handle(r); out.StatusCode != 400 {
+		t.Fail()
+	}
 }
 
 // for code coverage purposes only
