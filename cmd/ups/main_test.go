f9956d0ca3376527478d0cf1470fdb919fd97f98
nelsw <connorvanelswyk@gmail.com>
2020-06-03 13:10:52 -0400

    ups test update

--- cmd/ups/main_test.go
+++ cmd/ups/main_test.go
@@ -1,72 +1,28 @@
 package main
 
 import (
-	"hc-api/pkg/model/ups"
-	"hc-api/test"
+	"sam-app/pkg/model/ups"
+	"sam-app/test"
 	"testing"
 )
 
 func TestHandleRequest(t *testing.T) {
-	ty := ups.NodeType{Code: "03", Description: ""}
-	p := ups.Package{
-		Id:       test.ProductId,
-		ZipFrom:  "90210",
-		ZipTo:    "33401",
-		Weight:   7.5,
-		Width:    7,
-		Length:   10,
-		Height:   5,
-		NodeType: ty,
-		Dimensions: ups.Dimensions{
-			Type:   ups.NodeType{Code: "IN"},
-			Width:  "7",
-			Length: "10",
-			Height: "5",
-		},
-		WeightNode: ups.WeightNode{
-			Type:   ups.NodeType{Code: "LBS", Description: "Pounds"},
-			Weight: "7",
-		},
-	}
-	r := ups.PostageRateRequest{
-		Packages: []ups.Package{p},
-		//order
-		RateRequest: ups.RateRequest{
-			//package
-			Shipment: ups.Shipment{
-				//ShippingEntity
-				Shipper: ups.ShippingEntity{
-					Address: ups.Address{
-						PostalCode:  "90210",
-						CountryCode: "US",
-					},
-				},
-				//ShippingEntity
-				ShipTo: ups.ShippingEntity{
-					Address: ups.Address{
-						PostalCode:  "33401",
-						CountryCode: "US",
-					},
-				},
-				//ShippingEntity
-				ShipFrom: ups.ShippingEntity{
-					Address: ups.Address{
-						PostalCode:  "90210",
-						CountryCode: "US",
-					},
-				},
 
-				Service: ty,
+	lbs := ups.NodeType{"LBS", "Pounds"}
+	inc := ups.NodeType{"IN", "Inches"}
 
-				Weight: ups.WeightNode{
-					Type:   ups.NodeType{Code: "LBS", Description: "Pounds"},
-					Weight: "7.5",
-				},
+	svc := ups.NodeType{"03", "GROUND"}
+	w := ups.WeightNode{lbs, "7.5"}
+	d := ups.Dimensions{inc, "7", "10", "5"}
 
-				Package: p,
-			},
-		},
-	}
+	p := ups.Package{test.ProductId, svc, d, w}
+
+	fr := ups.ShippingEntity{ups.Address{"90210", "US"}}
+	to := ups.ShippingEntity{ups.Address{"33401", "US"}}
+
+	s := ups.Shipment{fr, to, fr, svc, w, p}
+
+	r := ups.PostageRateRequest{ups.RateRequest{s}, []ups.Package{p}}
 
 	if out, err := Handle(r); err != nil {
 		t.Fatal(err)
